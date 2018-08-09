585d7c61d034b5f1d5ee038c99f42f1bf2b6c325
Aleadinglight <huutam.ptnk@gmail.com>
2018-08-09 22:34:41 +0000

    Update firstContract.sol

--- firstContract.sol
+++ firstContract.sol
@@ -4,6 +4,7 @@ contract MyFirstContract{
     string private name;
     uint private age;
     
+    // The input must be of type JSON, remember to put the name between the quotes " "
     function setName(string newName){
         name = newName;
     }
