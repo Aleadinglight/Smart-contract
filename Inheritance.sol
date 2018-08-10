cb29feb1ea6cb973d57619667595fc78cc795898
Aleadinglight <huutam.ptnk@gmail.com>
2018-08-10 17:51:54 +0000

    Update Inheritance.sol

--- Inheritance.sol
+++ Inheritance.sol
@@ -1,6 +1,29 @@
 pragma solidity ^0.4.0;
 
-contract MyFirstContract{
+contract Bank {
+    // innernal : only MyFirstContract can access, not from outsite
+    // we have public, private, internal (protected)
+    uint private value;
+    
+    // Constructor
+    function Bank(uint amount){
+        value = amount;
+    }
+    
+    function deposit(uint amount){
+        value += amount;
+    }
+    
+    function withdraw(uint amount){
+        value -= amount;
+    }
+    
+    function balance() returns (uint){
+        return value;
+    }
+}
+
+contract MyFirstContract is Bank(100){
     string private name;
     uint private age;
     
